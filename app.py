0cb100e50ce77fabd1c09d3a934ff7491b212ba2
Sourav.banerjee <souravbanerjee0009@gmail.com>
2022-11-07 10:13:52 +0530

    saves

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
+import json
 import pickle
+
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-from numpy.core.numeric import outer
 import pandas as pd
 
 app=Flask(__name__)
-regmodel=pickle.load(open('regression.pkl','rb'))
-Scaler =pickle.load(open('scaling.pkl','rb'))
+## Load the model
+regression=pickle.load(open('regression.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
 @app.route('/')
-
 def home():
-    return render_template('home.html')
-
+    return render_template('hh.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
-    print(np.array(list(data.values()).reshape(1,-1)))
-    new_data=Scaler.transform(np.array(list(data.values()).reshape(1,-1)))
-    output= regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regression.predict(new_data)
     print(output[0])
-    return jsonify(outer[0])
+    return jsonify(output[0])
 
-if __name__=='__main__':
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regression.predict(final_input)[0]
+    return render_template("hh.html",prediction_text="The House price prediction is {}".format(output))
+   
+if __name__=="__main__":
     app.run(debug=True)
